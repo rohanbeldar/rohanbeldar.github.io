d14e03ea2f878c688276805cacf71e6cae472795
rohanbeldar <beldar.rohan@gmail.com>
2023-11-20 01:34:32 -0500

    Updates

---
+++ precache-manifest.da7aa29ec4eaca6575d89308c79f5bcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4d89c883b993ed45a5343f1a309069",
+    "url": "rohanbeldar.github.io/index.html"
+  },
+  {
+    "revision": "6abfd5907c050fa33637",
+    "url": "rohanbeldar.github.io/static/css/main.ff86c2cb.chunk.css"
+  },
+  {
+    "revision": "a470257653775c4102a7",
+    "url": "rohanbeldar.github.io/static/js/2.1afb2085.chunk.js"
+  },
+  {
+    "revision": "6abfd5907c050fa33637",
+    "url": "rohanbeldar.github.io/static/js/main.6a4782b2.chunk.js"
+  },
+  {
+    "revision": "c349a55505da9dbf0811",
+    "url": "rohanbeldar.github.io/static/js/runtime~main.0c271dce.js"
+  },
+  {
+    "revision": "c3ddee00598e7dec9f404574263746d4",
+    "url": "rohanbeldar.github.io/static/media/dp.c3ddee00.png"
+  },
+  {
+    "revision": "6acccdd597aea27fc3e5f994cb2937bb",
+    "url": "rohanbeldar.github.io/static/media/skyline.6acccdd5.jpg"
+  }
+]);
